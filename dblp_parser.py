69c38da382f51fb0dcb33fdebc1789cf6dd52e07
Konstantinos Christou <57042127+kostasc96@users.noreply.github.com>
2021-05-09 22:09:29 +0300

    load data into Neo4J database

--- dblp_parser.py
+++ dblp_parser.py
@@ -3,24 +3,43 @@ from lxml import etree
 from xml.etree.ElementTree import ParseError
 from lxml.etree import XMLSyntaxError
 import re
+from py2neo import Graph
+from py2neo import Node
+from py2neo import Relationship
 
 parser = etree.XMLParser(load_dtd=True)
 
 dtd = '<!DOCTYPE dblp SYSTEM "dblp.dtd">'
 
+graph = Graph(password="your_password")
+graph.delete_all()
+# graph.schema.create_uniqueness_constraint('Author', 'name')
+# graph.schema.create_uniqueness_constraint('Journal', 'name')
+try:
+    graph.schema.drop_index('Author', 'name')
+    graph.schema.drop_index('Journal', 'name')
+    graph.schema.drop_index('Book', 'name')
+except:
+    None
+graph.schema.create_index('Author', 'name')
+graph.schema.create_index('Journal', 'name')
+graph.schema.create_index('Book', 'name')
 
-def parser_articles(file_name, dtd1, num1):
+
+def parser_articles(file_name, dtd1, num1, parseAll):
     i=0
     writing = False
     start = "<article"
     end = "</article>"
     regexp1 = "</article><[^A-Z]+"
+    pattern1 = "[0-9]+-"
+    pattern2 = "-[0-9]+"
     dtd = dtd1
     xml = dtd
     with gzip.open(file_name,'rt') as f:
         for line in f:
             try:
-                if i > num1:
+                if i > num1 and parseAll == False:
                     break
                 #print(str(i) + ": " + line)
                 if end in line:
@@ -28,7 +47,7 @@ def parser_articles(file_name, dtd1, num1):
                     xml += line[:index]
                     try:
                         xmldoc = etree.fromstring(xml, parser=parser)
-                    except XMLSyntaxError:
+                    except:
                         xml = dtd
                         writing = False 
                         if start in line:
@@ -39,23 +58,48 @@ def parser_articles(file_name, dtd1, num1):
                     title = xmldoc.find(".//title").text
                     year = xmldoc.find(".//year").text
                     journal = xmldoc.find(".//journal").text
+                    pages = ""
+                    pageFrom = ""
+                    pageTo = ""
+                    numOfPages = 1
+                    if re.search("<pages>.*[0-9]+-[0-9]+.*</pages>", str(xml)):
+                        pages = re.search("<pages>.*[0-9]+-[0-9]+.*</pages>", str(xml)).group(0).replace('<pages>','').replace('</pages>','')
+                        pages = re.search("[0-9]+-[0-9]+", pages).group(0)
+                        pageFrom = re.search(pattern1, pages).group(0).replace('-','')
+                        pageTo = re.search(pattern2, pages).group(0).replace('-','')
+                        numOfPages = int(float(pageTo)) - int(float(pageFrom)) + 1
                     authors = []
+                    hasAuthors = False
                     if title:
                         for author in xmldoc.findall(".//author"):
-                            authors.append(author.text)
-                    if(title == None or year == None or journal == None or len(authors) == 0):
+                            hasAuthors = True
+                            authors.append(Node("Author", name=author.text))
+                    if(title == None or year == None or journal == None or hasAuthors == False):
                         if start in line:
                             writing = True
                             index = line.find(start)
                             xml += line[index:]
                         continue
+                    journalNode = Node("Journal",name=journal)
+                    journalNode.__primarylabel__ = "Journal"
+                    journalNode.__primarykey__ = "name"
+                    graph.merge(journalNode)
+                    articleNode = Node("Publication",title=title,year=year,pagesNo=numOfPages,category="article")
+                    graph.create(articleNode)
+                    for auth in authors:
+                        auth.__primarylabel__ = "Author"
+                        auth.__primarykey__ = "name"
+                        auth_node = graph.merge(auth)
+                        if auth_node != None:
+                            graph.create(Relationship(auth_node, "HAS_CONTRIBUTED", articleNode))
+                        else:
+                            graph.create(Relationship(auth, "HAS_CONTRIBUTED", articleNode))
+                    graph.create(Relationship(journalNode,"HAS_ARTICLE",articleNode))
                     i+=1
-                    print("Article")
-                    #print(str(i) + "\n" + "title: " + title)
-                    print(str(i) + " \n" + "title:" + title + " \n" + "year:" + year + " \n" + "journal:" +journal)
-                    for author in authors:
-                        print(author)
-                    print("--------------------------------")
+                    # print(str(i) + " \n" + "title:" + title + " \n" + "year:" + year + " \n" + "journal:" +journal + "\n" + "numOfPages:" + str(numOfPages))
+                    # for author in authors:
+                    #     print(author)
+                    print("Article " + str(i) + " --------------------------------")
                     xml = dtd
                     writing = False
 
@@ -71,7 +115,7 @@ def parser_articles(file_name, dtd1, num1):
 
 
 
-def parser_inproceedings(file_name, dtd1, num1):
+def parser_inproceedings(file_name, dtd1, num1, parseAll):
     i=0
     writing = False
     start2 = "<inproceedings"
@@ -84,7 +128,7 @@ def parser_inproceedings(file_name, dtd1, num1):
     with gzip.open(file_name,'rt') as f:
         for line in f:
             try:
-                if i > num1:
+                if i > num1 and parseAll == False:
                     break
                 #print(str(i) + ": " + line)
                 if end2 in line:
@@ -114,21 +158,37 @@ def parser_inproceedings(file_name, dtd1, num1):
                         pageTo = re.search(pattern2, pages).group(0).replace('-','')
                         numOfPages = int(float(pageTo)) - int(float(pageFrom)) + 1
                     authors = []
+                    hasAuthors = False
                     if title:
                         for author in xmldoc.findall(".//author"):
-                            authors.append(author.text)
-                    if(title == None or year == None or booktitle == None or len(authors) == 0):
+                            hasAuthors = True
+                            authors.append(Node("Author", name=author.text))
+                    if(title == None or year == None or booktitle == None or hasAuthors == False):
                         if start2 in line:
                             writing = True
                             index2 = line.find(start2)
                             xml += line[index2:]
                         continue
+                    bookNode = Node("Book",name=booktitle)
+                    bookNode.__primarylabel__ = "Book"
+                    bookNode.__primarykey__ = "name"
+                    graph.merge(bookNode)
+                    inproceedingNode = Node("Publication",title=title,year=year,pagesNo=numOfPages,category="inproceeding")
+                    graph.create(inproceedingNode)
+                    for auth in authors:
+                        auth.__primarylabel__ = "Author"
+                        auth.__primarykey__ = "name"
+                        auth_node = graph.merge(auth)
+                        if auth_node != None:
+                            graph.create(Relationship(auth_node, "HAS_CONTRIBUTED", inproceedingNode))
+                        else:
+                            graph.create(Relationship(auth, "HAS_CONTRIBUTED", inproceedingNode))
+                    graph.create(Relationship(bookNode,"HAS_PUBLICATION",inproceedingNode))
                     i+=1
-                    print("Inproceeding")
-                    print(str(i) + " \n" + "title:" + title + " \n" + "year:" + year + " \n" + "booktitle:" +booktitle + "\n" + "numOfPages:" + str(numOfPages))
-                    for author in authors:
-                        print(author)
-                    print("--------------------------------")
+                    print("Inproceeding " + str(i) + " --------------------------------")
+                    # print(str(i) + " \n" + "title:" + title + " \n" + "year:" + year + " \n" + "booktitle:" +booktitle + "\n" + "numOfPages:" + str(numOfPages))
+                    # for author in authors:
+                    #     print(author)
                     xml = dtd
                     writing = False
                 
@@ -144,7 +204,7 @@ def parser_inproceedings(file_name, dtd1, num1):
 
 
 
-def parser_incollections(file_name, dtd1, num1):
+def parser_incollections(file_name, dtd1, num1, parseAll):
     i=0
     writing = False
     start3 = "<incollection"
@@ -157,7 +217,7 @@ def parser_incollections(file_name, dtd1, num1):
     with gzip.open(file_name,'rt') as f:
         for line in f:
             try:
-                if i > num1:
+                if i > num1 and parseAll == False:
                     break
                 #print(str(i) + ": " + line)
                 if end3 in line:
@@ -187,21 +247,37 @@ def parser_incollections(file_name, dtd1, num1):
                         pageTo = re.search(pattern2, pages).group(0).replace('-','')
                         numOfPages = int(float(pageTo)) - int(float(pageFrom)) + 1
                     authors = []
+                    hasAuthors = False
                     if title:
                         for author in xmldoc.findall(".//author"):
-                            authors.append(author.text)
-                    if(title == None or year == None or booktitle == None or len(authors) == 0):
+                            hasAuthors = True
+                            authors.append(Node("Author", name=author.text))
+                    if(title == None or year == None or booktitle == None or hasAuthors == False):
                         if start3 in line:
                             writing = True
                             index3 = line.find(start3)
                             xml += line[index3:]
                         continue
+                    bookNode = Node("Book",name=booktitle)
+                    bookNode.__primarylabel__ = "Book"
+                    bookNode.__primarykey__ = "name"
+                    graph.merge(bookNode)
+                    incollectionNode = Node("Publication",title=title,year=year,pagesNo=numOfPages,category="incollection")
+                    graph.create(incollectionNode)
+                    for auth in authors:
+                        auth.__primarylabel__ = "Author"
+                        auth.__primarykey__ = "name"
+                        auth_node = graph.merge(auth)
+                        if auth_node != None:
+                            graph.create(Relationship(auth_node, "HAS_CONTRIBUTED", incollectionNode))
+                        else:
+                            graph.create(Relationship(auth, "HAS_CONTRIBUTED", incollectionNode))
+                    graph.create(Relationship(bookNode,"HAS_PUBLICATION",incollectionNode))
                     i+=1
-                    print("Incollection")
-                    print(str(i) + " \n" + "title:" + title + " \n" + "year:" + year + " \n" + "booktitle:" +booktitle + "\n" + "numOfPages:" + str(numOfPages))
-                    for author in authors:
-                        print(author)
-                    print("--------------------------------")
+                    print("Incollection " + str(i) + " --------------------------------")
+                    # print(str(i) + " \n" + "title:" + title + " \n" + "year:" + year + " \n" + "booktitle:" +booktitle + "\n" + "numOfPages:" + str(numOfPages))
+                    # for author in authors:
+                    #     print(author)
                     xml = dtd
                     writing = False
 
@@ -216,6 +292,6 @@ def parser_incollections(file_name, dtd1, num1):
                 continue
 
 
-parser_articles('dblp.xml.gz',dtd,1000)
-#parser_inproceedings('dblp.xml.gz',dtd,1000)
-#parser_incollections('dblp.xml.gz',dtd,1000)
+parser_articles('dblp.xml.gz',dtd,1000,False)
+parser_inproceedings('dblp.xml.gz',dtd,1000,False)
+parser_incollections('dblp.xml.gz',dtd,1000,False)
